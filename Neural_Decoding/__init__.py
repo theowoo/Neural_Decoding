f44112b1fc01592f644c910bf1bf25fff100a7b0
theowoo <theowoo@users.noreply.github.com>
2025-10-16 14:54:44 +0200

    flake and black clean

--- Neural_Decoding/__init__.py
+++ Neural_Decoding/__init__.py
@@ -1,4 +1,18 @@
-from .decoders import WienerFilterDecoder, WienerCascadeDecoder, KalmanFilterDecoder,\
-            DenseNNDecoder, SimpleRNNDecoder, GRUDecoder, LSTMDecoder, XGBoostDecoder, SVRDecoder, NaiveBayesDecoder
-from .metrics import get_R2, get_rho
-from .preprocessing_funcs import bin_output, bin_spikes, get_spikes_with_history
+from .decoders import (  # noqa: F401
+    DenseNNDecoder,
+    GRUDecoder,
+    KalmanFilterDecoder,
+    LSTMDecoder,
+    NaiveBayesDecoder,
+    SimpleRNNDecoder,
+    SVRDecoder,
+    WienerCascadeDecoder,
+    WienerFilterDecoder,
+    XGBoostDecoder,
+)
+from .metrics import get_R2, get_rho  # noqa: F401
+from .preprocessing_funcs import (  # noqa: F401
+    bin_output,
+    bin_spikes,
+    get_spikes_with_history,
+)
